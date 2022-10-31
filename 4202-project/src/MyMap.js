f3119d0095b18b321b84b4a9782578f0297f6218
Saif Rahman <saif9817@hotmail.com>
2022-10-31 10:05:49 -0400

    updated to make use of react-leaflet

--- 4202-project/src/MyMap.js
+++ 4202-project/src/MyMap.js
@@ -1,21 +1,35 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker} from "react-leaflet";
-import {useMap} from "react-leaflet/hooks";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 
 export default class MyMap extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       lat: 45.4215,
-      lng: 75.6972,
-      zoom: 7,
+      lng: -75.6972,
+      zoom: 14,
     };
   }
 
   render() {
     const position = [this.state.lat, this.state.lng];
+    const centre = [45.40226, -75.68882];
     return (
-      <MapContainer center={position} zoom={this.state.zoom}>
+      <MapContainer center={centre} zoom={this.state.zoom} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            Ottawa.
+          </Popup>
+        </Marker>
+        <Marker position={[45.3831, -75.6976]}>
+          <Popup>
+            Carleton University
+          </Popup>
+        </Marker>
       </MapContainer>
     );
   }
